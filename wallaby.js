e7931f7df22c66c67af2c21895d52cfb1d230d43
Michael Salaverry <michaels@wix.com>
2017-06-28 11:17:40 +0300

    added many h6 headers instead of p text in order to have a better html5 outline
    Signed-off-by: Michael Salaverry <michaels@wix.com>

---
+++ wallaby.js
@@ -0,0 +1,14 @@
+/**
+ * Created by michaels on 28/06/2017.
+ */
+module.exports = function () {
+  return {
+    files: [
+      'src/**/*.js'
+    ],
+
+    tests: [
+      'test/**/*Spec.js'
+    ]
+  };
+};
